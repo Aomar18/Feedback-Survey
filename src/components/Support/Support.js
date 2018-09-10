5c1f73ef0334944a276f214ebca3e6ee41bb8b2f
Ahmed <omar0121@umn.edu>
2018-09-09 21:49:07 -0500

    setup complete for support component

--- src/components/Support/Support.js
+++ src/components/Support/Support.js
@@ -6,13 +6,69 @@ const emptyfeedback = {
 }
 
 class Support extends Component {
+    constructor() {
+        super();
+
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            ...this.state,
+            support: event.target.value
+        });
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        const action = { type: 'ADD_SUPPORT', payload: this.state }
+        this.props.dispatch(action);
+        this.props.history.push('support');
+        this.emptyInputs();
+    }
+
+    emptyInputs()
 
 
-render() {
-    return(
-        <div></div>
-    )
+
+
+    render() {
+        return (<div className="supportView">
+
+            <form className="supportSurvey">
+
+                <input className='radio' onChange={this.handleChange}
+                    checked="{this.state.support === 1}"
+                    type="radio" value="1" />
+
+                <input className='radio' onChange={this.handleChange}
+                    checked="{this.state.support === 2}"
+                    type="radio" value="2" />
+
+                <input className='radio' onChange={this.handleChange}
+                    checked="{this.state.support === 3}"
+                    type="radio" value="3" />
+
+                <input className='radio' onChange={this.handleChange}
+                    checked="{this.state.support === 4}"
+                    type="radio" value="4" />
+
+                <input className='radio' onChange={this.handleChange}
+                    checked="{this.state.support === 5}"
+                    type="radio" value="5" />
+
+                <input type="submit" onClick={this.handleSubmit}>NEXT</input>
+
+
+
+            </form>
+        </div>
+        
+        )
+    }
+
 }
-}
 
-export default Support;
+const mapReduxStateToProps = (reduxState) => ({
+    reduxState
+})
+export default connect(mapReduxStateToProps)(support);
