08b2b3d6dbdeee3c69d3d872e119ff4093aec7d1
Ahmed <omar0121@umn.edu>
2018-09-10 00:21:36 -0500

    base complete

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,16 +1,84 @@
 import React, { Component } from 'react';
-import {connect} from 'react-redux'
+import axios from 'axios';
+import { connect } from 'react-redux';
+const emptyInfo = [];
+
+
 class Admin extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            feedback: emptyInfo
+
+        }
+    }
+
+    componentDidMount() {
+        this.getResults();
+    }
+
+    getResults = () => {
+        axios({
+            method: 'GET',
+            url: '/feedback',
+        }).then((response) => {
+            console.log(response.data);
+            this.setState({
+                result: response.data.rows
+            })
+        }).catch((error) => {
+            console.log('error in get', error);
+        })
+    }
 
 
-render(){
-    return(
-        <div></div>
-    )
+    handleDelete = () => {
+        axios({
+            method: 'DELETE',
+            url: `/feedback/${this.props.result.id}`,
+        }).then((response) => {
+            this.props.getResults();
+        }).catch((error) => {
+            console.log('error making Delete request', error);
+        });
+
+    }
+
+    render() {
+        return (
+            <div>
+                <table align="center" className="table">
+                    <thead>
+                        <tr>
+                            <th>Feeling</th>
+                            <th>Comprehension</th>
+                            <th>Support</th>
+                            <th>Comments</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>{this.props.result.feeling}</td>
+                            <td>{this.props.result.understanding} </td>
+                            <td>{this.props.result.support}</td>
+                            <td>{this.props.result.comments}</td>
+                            <td><button onClick={this.handleDelete}>Delete</button></td>
+                        </tr>
+
+                    </tbody>
+                </table>
+
+            </div>
+        )
+    }
+
 }
 
-}
 
 
-
-export default Admin;
+const mapReduxStateToProps = (reduxState) => ({
+    reduxState
+});
+export default connect(mapReduxStateToProps)(Admin);
